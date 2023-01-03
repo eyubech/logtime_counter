04c8e0890853903e35b98f8099c66cc39ac61ee1
Ayoub Ech-chetyouy <76597998+eyubech@users.noreply.github.com>
2023-01-03 19:39:06 +0100

    update script

--- run.sh
+++ run.sh
@@ -1,13 +1,8 @@
 cd
-
 mkdir logtime
 cd logtime
 git clone git@github.com:eyubech/logtime_counter.git
-
 cd
-
-
-
 python3 -m pip install requests
-
 echo 'alias mylog="python3 ~/logtime/logtime_counter/main.py"' >> .zshrc
+echo "Thanks for using my script :)"
