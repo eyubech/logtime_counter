740921f476dddb3dc6e8a065a505f2628bc835b7
Ayoub Ech-chetyouy <76597998+eyubech@users.noreply.github.com>
2023-01-03 18:37:09 +0100

    ruuner script

---
+++ run.sh
@@ -0,0 +1,13 @@
+cd
+
+mkdir logtime
+cd logtime
+git clone git@github.com:eyubech/logtime_counter.git
+
+cd
+
+
+
+pip3 install requests
+
+echo "alias mylog="python3 ~/logtime/logtime_counter/main.py"" >> .zshrc
