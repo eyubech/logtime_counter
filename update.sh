05ab5f3b390ff245b02f41e43f46d6738d52e6bc
Ayoub Ech-chetyouy <76597998+eyubech@users.noreply.github.com>
2023-01-04 13:45:30 +0100

    update script

---
+++ update.sh
@@ -0,0 +1,9 @@
+cd
+cd logtime_count
+rm -rf logtime_counter
+git clone git@github.com:eyubech/logtime_counter.git
+echo ""
+echo "******************************"
+echo "     Updated successfully    "
+echo "******************************"
+echo ""
